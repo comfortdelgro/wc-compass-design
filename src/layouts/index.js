f7d70a031ac5a9dd433d6c8f41b99c8fe995d4bc
DuongTX <duongtx@fsoft.com.vn>
2023-02-23 09:32:56 +0700

    Add card components and publish new page

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,1 +1,5 @@
 export { CdgActionBar } from './action-bar/action-bar';
+export { CdgCard } from './card/card';
+export { CdgCardHeader } from './card/card-header';
+export { CdgCardBody } from './card/card-body';
+export { CdgCardActions } from './card/card-actions';
