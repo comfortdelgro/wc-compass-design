64a3fe246e4940c4ffc934dc428552f9aad337f4
JOBKO\tudq <tudq@jobkorea.co.kr>
2023-03-01 14:57:53 +0700

    Trigger 5

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -64,6 +64,7 @@ export class CdgDropdown extends HTMLElement {
     console.log('dropdown....111');
     console.log('dropdown....222');
     console.log('dropdown....333');
+    console.log('dropdown....444');
     super();
     this.prepend(template.content.cloneNode(true));
     this.contentElement = document.createElement('cdg-dropdown-select');
