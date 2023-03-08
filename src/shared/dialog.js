6ee10bc55c3c9040b10514e3372b03dc26587803
fpt-duongtx <119040724+fpt-duongtx@users.noreply.github.com>
2023-03-08 16:36:48 +0700

    feat(dialog): Add dialog module (#44)
    * Add dialog module
    
    * Add dialog module code build

--- src/shared/dialog.js
+++ src/shared/dialog.js
@@ -8,14 +8,71 @@ export class DialogService {
     document.body.appendChild(modalElement);
   }
 
-  hide(id) {
+  alert({ dialogTitle = '', message = '', buttonLabel = 'Okay' }) {
+    const alert = document.createElement('cdg-dialog-alert');
+    alert.setAttribute('dialogTitle', dialogTitle);
+    alert.setAttribute('message', message);
+    alert.setAttribute('executeLabel', buttonLabel);
+    this.show('alert' + new Date().getTime(), alert);
+    return alert;
+  }
+
+  success(title, message) {
+    console.log('Show alert with title & message');
+  }
+
+  error(title, message) {
+    console.log('Show alert with title & message');
+  }
+
+  confirm({
+    dialogTitle = '',
+    message = '',
+    executeLabel = 'Okay',
+    cancelLabel = 'Cancel',
+  }) {
+    const alert = document.createElement('cdg-dialog-confirm');
+    alert.setAttribute('dialogTitle', dialogTitle);
+    alert.setAttribute('message', message);
+    alert.setAttribute('executeLabel', executeLabel);
+    alert.setAttribute('cancelLabel', cancelLabel);
+    this.show('alert' + new Date().getTime(), alert);
+    return alert;
+  }
+
+  confirmCustom({ content, executeLabel = 'Okay', cancelLabel = 'Cancel' }) {
+    const alert = document.createElement('cdg-dialog-confirm');
+    alert.appendChild(content);
+    alert.setAttribute('useCustomContent', 'true');
+    alert.setAttribute('executeLabel', executeLabel);
+    alert.setAttribute('cancelLabel', cancelLabel);
+    this.show('alert' + new Date().getTime(), alert);
+    return alert;
+  }
+
+  warning({
+    dialogTitle = '',
+    message = '',
+    executeLabel = 'Okay',
+    cancelLabel = 'Cancel',
+  }) {
+    const alert = document.createElement('cdg-dialog-warning');
+    alert.setAttribute('dialogTitle', dialogTitle);
+    alert.setAttribute('message', message);
+    alert.setAttribute('executeLabel', executeLabel);
+    alert.setAttribute('cancelLabel', cancelLabel);
+    this.show('alert' + new Date().getTime(), alert);
+    return alert;
+  }
+
+  hide(id, answer) {
     if (!this.modals[id]) {
       console.error('Can not find this id');
       return;
     }
 
     this.modals[id].firstElementChild.dispatchEvent(
-      new CustomEvent('close', { detail: '' })
+      new CustomEvent('close', { detail: answer })
     );
 
     document.body.removeChild(this.modals[id]);
