a22135a6836aa557b4c4770e0c496a77753cfa23
fpt-duongtx <119040724+fpt-duongtx@users.noreply.github.com>
2023-03-14 21:08:08 +0700

    Feature/video player (#68)
    * Add video player
    
    * Update for video player

--- src/components/range-slider/range-slider.js
+++ src/components/range-slider/range-slider.js
@@ -1,4 +1,8 @@
 export class CdgRangeSlider extends HTMLElement {
+  static get observedAttributes() {
+    return ['value', 'buffering'];
+  }
+
   get width() {
     return this.offsetWidth - this.thumb.offsetWidth;
   }
@@ -7,24 +11,40 @@ export class CdgRangeSlider extends HTMLElement {
   get value() {
     return Number(this.getAttribute('value'));
   }
+
   set value(val) {
     this.setAttribute('value', val);
   }
+
+  get buffering() {
+    return Number(this.getAttribute('buffering'));
+  }
+
+  set buffering(buffering) {
+    this.setAttribute('buffering', buffering);
+  }
+
   get min() {
     return Number(this.getAttribute('min'));
   }
+
   set min(val) {
     this.setAttribute('min', val);
   }
+
   get max() {
     return Number(this.getAttribute('max'));
   }
+
   set max(val) {
     this.setAttribute('max', val);
   }
 
   thumb;
   slider;
+  dragging;
+  bufferingBar;
+
   constructor() {
     super();
 
@@ -34,6 +54,8 @@ export class CdgRangeSlider extends HTMLElement {
 
     this.slider = document.createElement('div');
     this.slider.classList.add('range-slider');
+
+    this.updateBufferingBar();
 
     this.thumb = document.createElement('div');
     this.thumb.classList.add('thumb');
@@ -45,13 +67,27 @@ export class CdgRangeSlider extends HTMLElement {
     // Initialize state
     this.min = this.getAttribute('min') || 0;
     this.max = this.getAttribute('max') || 100;
-    this.value = this.getAttribute('value') || 50;
+    this.value = this.getAttribute('value') || 0;
     this.updatePosition();
     this.thumb.setAttribute('value', this.getAttribute('value'));
     // Bind event listeners
     this.thumb.addEventListener('pointerdown', this.handleMouseDown.bind(this));
     this.addEventListener('click', this.handleClick.bind(this));
     // let isMouseDown = fasle;
+  }
+
+  updateBufferingBar() {
+    if (!this.buffering) {
+      return;
+    }
+
+    if (!this.bufferingBar) {
+      this.bufferingBar = document.createElement('div');
+      this.bufferingBar.classList.add('cdg-buffering-bar');
+      this.appendChild(this.bufferingBar);
+    }
+
+    this.bufferingBar.style.width = this.buffering + '%';
   }
 
   // Dispatch a custom event with the new value whenever it changes
@@ -65,12 +101,28 @@ export class CdgRangeSlider extends HTMLElement {
     this.dispatchEvent(event);
   }
 
+  attributeChangedCallback(attr) {
+    switch (attr) {
+      case 'value':
+        if (!this.dragging) {
+          this.updatePosition();
+        }
+        break;
+
+      case 'buffering':
+        this.updateBufferingBar();
+        break;
+
+      default:
+        break;
+    }
+  }
+
   // Update thumb position based on current value
   updatePosition() {
     this.thumb.setAttribute('value', this.getAttribute('value'));
     const percent = (this.value - this.min) / (this.max - this.min);
     this.slider.style.width = percent * this.width + 'px';
-    this.dispatchChangeEvent();
   }
 
   // Handle thumb drag
@@ -82,18 +134,21 @@ export class CdgRangeSlider extends HTMLElement {
     const thumbRect = this.thumb.getBoundingClientRect();
     const thumbX = thumbRect.left - sliderRect.left;
     const moveHandler = (event) => {
+      this.dragging = true;
       const deltaX = event.clientX - startX;
       const newThumbX = Math.min(Math.max(thumbX + deltaX, 0), this.width);
       const percent = newThumbX / this.width;
       this.value = Math.round(percent * (this.max - this.min) + this.min);
       this.updatePosition();
+      this.dispatchChangeEvent();
     };
     const upHandler = () => {
-      document.removeEventListener('pointermove', moveHandler);
-      document.removeEventListener('pointerup', upHandler);
+      this.dragging = false;
+      this.removeEventListener('pointermove', moveHandler);
+      this.removeEventListener('pointerup', upHandler);
     };
-    document.addEventListener('pointermove', moveHandler);
-    document.addEventListener('pointerup', upHandler);
+    this.addEventListener('pointermove', moveHandler);
+    this.addEventListener('pointerup', upHandler);
   }
 
   // Handle click on slider
@@ -104,6 +159,7 @@ export class CdgRangeSlider extends HTMLElement {
       const percent = event.offsetX / this.width;
       this.value = Math.round(percent * (this.max - this.min) + this.min);
       this.updatePosition();
+      this.dispatchChangeEvent();
     }
   }
 }
